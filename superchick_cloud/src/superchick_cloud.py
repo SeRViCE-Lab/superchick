8f90c6cb62b2b42d46c207df2cac82495ad8e7f3
lakehanne <patlekano@gmail.com>
2016-10-03 21:24:15 -0500

    vicon_point_cloud data

--- superchick_cloud/src/superchick_cloud.py
+++ superchick_cloud/src/superchick_cloud.py
@@ -2,54 +2,65 @@
 
 '''
 	/*
-	*  
-	\	This code is part of the superchick project. 
-	\ 	Essentially, we are transforming the geometry_msgs 
-	\ 	transformed data to pointcloud2 messages.
-	\
-	\ 
-	\	Author: Olalekan Ogunmolu
-	\	Date: Oct 2, 2016
+	*\	This code is part of the superchick project. 
+	*\ 	Essentially, we are transforming the geometry_msgs 
+	*\ 	transformed data to pointcloud2 messages.
+	*\ 
+	*\	Author: Olalekan Ogunmolu
+	*\	Date: Oct 2, 2016
 	*/
 '''
 
+# globals
+
 import rospy
-from sensor_msgs.msg import PointCloud2
+
 from geometry_msgs.msg import TransformStamped
 from vicon_bridge.msg import Markers
 from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud 
-from geometry_msgs.msg import do_transform_point
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion, PointStamped, Vector3Stamped, Vector3
+
+import std_msgs.msg
+from sensor_msgs.msg import PointCloud2
+import sensor_msgs.point_cloud2 as pcl2
 
 def callback(markers):
-	# rospy.loginfo(rospy.get_caller_id() + "\nSuperdude markers: \n%s \n", markers.markers)
+	# rospy.loginfo(rospy.get_caller_id() + "\nSuperdude markers: \n%s \n", markers.markers)		
 	fore_marker 	= markers.markers[0]
 	left_marker 	= markers.markers[1]
 	right_marker 	= markers.markers[2]
 	chin_marker 	= markers.markers[3]
 
-	print("fore_marker: ", fore_marker)
-	# print "\n"
-	print("left_marker: ", left_marker)
-	# print "\n"
-	print("right_marker: " ,right_marker)
-	# print "\n"
-	print("chin_marker: ", chin_marker)
-	# print "\n"
-
 	geometry_to_cloud2(fore_marker, left_marker, right_marker, chin_marker)
 
 def geometry_to_cloud2(fore, left, right, chin):
-	fore_cloud 	= do_transform_point(fore, transform)
-	left_cloud 	= do_transform_point(left, transform)
-	right_cloud = do_transform_point(right, transform)
-	chin_cloud 	= do_transform_point(chin, transform)
 
+	pcl_pub = rospy.Publisher("/vicon_clouds", PointCloud2)
+	rospy.loginfo("==> initialting publisher")
+	rospy.sleep(1.)
+	clouds = [ 	
+				[(fore.translation.x)/100, (fore.translation.y)/100, (fore.translation.z)/100], 
+				[(left.translation.x)/100, (left.translation.y)/100, (left.translation.z)/100], 
+				[(right.translation.x)/100, (right.translation.y)/100, (right.translation.z)/100], 
+				[(chin.translation.x)/100, (chin.translation.y)/100, (chin.translation.z)/100] 
+			]
+	print "===> clouds\n"
+	print(clouds)
 
+	#cloud_header
+	header = std_msgs.msg.Header()
+	header.stamp = rospy.Time.now()
+	header.frame_id = 'cloud_map'
+
+	#create pcl from points
+	scaled_pcl = pcl2.create_cloud_xyz32(header, clouds)
+	#publish    
+	rospy.loginfo("happily publishing vicon pointcloud.. !")
+	pcl_pub.publish(scaled_pcl)
 
 
 def markers_listener():
 	rospy.init_node('super_listener', anonymous=True)
-
 	markers = rospy.Subscriber("/vicon/markers", Markers, callback)
 
 	rospy.spin()
