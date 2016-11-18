178ba276fbbea64f04e6c4654cf5edc61288f609
lakehanne <patlekano@gmail.com>
2016-11-18 15:10:32 -0600

    updated chick

--- jointpub/src/jointpub.cpp
+++ jointpub/src/jointpub.cpp
@@ -33,50 +33,41 @@ int main(int argc, char** argv)
         joint_state.name[0] ="table_to_bladder";
         joint_state.position[0] = 0; //table;
         joint_state.name[1] ="bladder_to_headnball";
-        joint_state.position[1] = 0.15; //bladder;
+        joint_state.position[1] = 0; //bladder;
         joint_state.name[2] ="base_to_right_leg";
-        joint_state.position[2] =  0.3;
+        joint_state.position[2] =  0;
         joint_state.name[3] ="base_to_left_leg";
-        joint_state.position[3] = 0.3;
+        joint_state.position[3] = 0;
         joint_state.name[4] ="rightleg_to_tabletop";
-        joint_state.position[4] = 0.35;
+        joint_state.position[4] = 0.0;
         joint_state.name[5] ="tablecover_to_tabletop";
-        joint_state.position[5] = -0.08;
+        joint_state.position[5] = -0;
         joint_state.name[6] ="table_to_bladder";
         joint_state.position[6] = 0;
         joint_state.name[7] ="base_panel_to_table_top";
-        joint_state.position[7] = -0.18;
+        joint_state.position[7] = 0;
         joint_state.name[8] ="vicon_to_world";
-        joint_state.position[8] = 2.54;
+        joint_state.position[8] = 0;
         joint_state.name[9] ="panel_12_oc_to_base_panel";
-        joint_state.position[9] = .200025/2;
+        joint_state.position[9] = 0;
         joint_state.name[10] ="panel_9_oc_to_panel_12_oc";
         joint_state.position[10] = 0;
         joint_state.name[11] ="panel_3_oc_to_panel_12_oc";
         joint_state.position[11] = 0;
         joint_state.name[12] ="leftleg_to_tabletop";
-        joint_state.position[12] = 0.35;
-
-
-        // update transform
-        // (moving in a circle with radius=2)
-        head_trans.header.stamp = ros::Time::now();
-        head_trans.transform.translation.x = cos(angle)*2;
-        head_trans.transform.translation.y = sin(angle)*2;
-        head_trans.transform.translation.z = 0; 
-        head_trans.transform.rotation = tf::createQuaternionMsgFromYaw(angle+M_PI/2);
+        joint_state.position[12] = 0.0;
+        joint_state.name[13] = "table_to_big_bladder";
+        joint_state.name[14] = "table_to_rightneckbladder";
+        joint_state.name[15] = "table_to_leftneckbladder";
+        joint_state.name[16] = "bigbladder_to_head";
+        joint_state.name[17] = "rightbladder_to_neck";
+        joint_state.name[18] = "leftbladder_to_neck";
+        joint_state.name[19] = "tableTop_to_basePanel";
+        joint_state.name[20] = "vicon_to_world";
 
         //send the joint state and transform
         joint_pub.publish(joint_state);
         broadcaster.sendTransform(head_trans);
-
-        // Create new robot state
-        // bladder += tinc;
-        // if (bladder<-1.0 || bladder>1) tinc *= -1;
-        // height += hinc;
-        // if (height>.5 || height<0) hinc *= -1;
-        // table += degree;
-        // angle += degree/4;
 
         // This will adjust as needed per iteration
         loop_rate.sleep();
