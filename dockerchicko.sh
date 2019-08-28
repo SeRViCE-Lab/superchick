93ff3ceb5beed58f6012297a1fc8beed0319d8d3
lakehanne <patlekano@gmail.com>
2019-08-27 22:21:52 -0500

    cleaned up on apple

--- dockerchicko.sh
+++ dockerchicko.sh
@@ -17,7 +17,7 @@ elif [[ "$(uname -s)" == 'Darwin' ]]; then
   # echo -e "ip: ${ip}"
   xhost + $ip
   docker run --privileged -it --rm -e DISPLAY=${ip}:1 -v /tmp/.X11-unix:/tmp/.X11-unix \
-        -v ~/Documents/superchicko:/opt/ros2_ws/src/superchicko $IMAGE
+        -v ~/Documents/superchicko:/root/superchicko:rw $IMAGE
   # docker run --privileged -it --rm -e DISPLAY=${ip}:0 -v /tmp/.X11-unix:/tmp/.X11-unix \
   #       -v ~/Documents/superchicko:/root/superchicko jess/firefox
   # docker run -d --name IAB DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix ~/Documents/superchicko:/opt/ros_ws/ lakehanne/sofa:IAB
--- sofa/plugins/IABPlugin/include/initIABPlugin.h
+++ sofa/plugins/IABPlugin/include/initIABPlugin.h
@@ -1,38 +1,14 @@
-/******************************************************************************
-*       SOFA, Simulation Open-Framework Architecture, development version     *
-*                (c) 2006-2019 INRIA, USTL, UJF, CNRS, MGH                    *
-*                                                                             *
-* This program is free software; you can redistribute it and/or modify it     *
-* under the terms of the GNU Lesser General Public License as published by    *
-* the Free Software Foundation; either version 2.1 of the License, or (at     *
-* your option) any later version.                                             *
-*                                                                             *
-* This program is distributed in the hope that it will be useful, but WITHOUT *
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
-* for more details.                                                           *
-*                                                                             *
-* You should have received a copy of the GNU Lesser General Public License    *
-* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
-*******************************************************************************
-* Authors: The SOFA Team and external contributors (see Authors.txt)          *
-*                                                                             *
-* Contact information: contact@sofa-framework.org                             *
-******************************************************************************/
-#include <IABPlugin/include/config.h>
 
-extern "C" {
+#ifndef INITIABPLUGIN_H
+#define INITIABPLUGIN_H
 
-SOFA_IABPLUGIN_API void initExternalModule();
+#include <sofa/helper/system/config.h>
 
-SOFA_IABPLUGIN_API const char* getModuleName();
+#ifdef SOFA_BUILD_IABPLUGIN
+#define SOFA_IABPlugin_API SOFA_EXPORT_DYNAMIC_LIBRARY
+#else
+#define SOFA_IABPlugin_API SOFA_IMPORT_DYNAMIC_LIBRARY
+#endif
 
-SOFA_IABPLUGIN_API const char* getModuleVersion();
 
-SOFA_IABPLUGIN_API const char* getModuleLicense();
-
-SOFA_IABPLUGIN_API const char* getModuleDescription();
-
-SOFA_IABPLUGIN_API const char* getModuleComponentList();
-
-}
+#endif
