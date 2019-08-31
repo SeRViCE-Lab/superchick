8aa90f0835fa65fadca9876d2f1eb14cafe55bd5
lakehanne <patlekano@gmail.com>
2019-08-31 12:51:17 -0500

    added working srs scene files

---
+++ sofa/build/build/libs/CMakeFiles/IABPlugin.dir/DependInfo.cmake
@@ -0,0 +1,69 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/superchicko/sofa/build/build/libs/include/moc_DataWidgetUnsigned.cpp" "/root/superchicko/sofa/build/build/libs/CMakeFiles/IABPlugin.dir/include/moc_DataWidgetUnsigned.cpp.o"
+  "/root/superchicko/sofa/plugins/IABPlugin/src/BehaviorModel.cpp" "/root/superchicko/sofa/build/build/libs/CMakeFiles/IABPlugin.dir/src/BehaviorModel.cpp.o"
+  "/root/superchicko/sofa/plugins/IABPlugin/src/DataWidgetUnsigned.cpp" "/root/superchicko/sofa/build/build/libs/CMakeFiles/IABPlugin.dir/src/DataWidgetUnsigned.cpp.o"
+  "/root/superchicko/sofa/plugins/IABPlugin/src/MappingPendulumInPlane.cpp" "/root/superchicko/sofa/build/build/libs/CMakeFiles/IABPlugin.dir/src/MappingPendulumInPlane.cpp.o"
+  "/root/superchicko/sofa/plugins/IABPlugin/src/ProjectiveConstraintSet.cpp" "/root/superchicko/sofa/build/build/libs/CMakeFiles/IABPlugin.dir/src/ProjectiveConstraintSet.cpp.o"
+  "/root/superchicko/sofa/plugins/IABPlugin/src/initIABPlugin.cpp" "/root/superchicko/sofa/build/build/libs/CMakeFiles/IABPlugin.dir/src/initIABPlugin.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "IAB_ROOT=/root/superchicko/sofa"
+  "NDEBUG"
+  "QT_CHARTS_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_WIDGETS_LIB"
+  "QT_XML_LIB"
+  "SOFAGUIQT_HAS_QTCHARTS"
+  "SOFA_ROOT=/sofa/build"
+  "USING_DOUBLE"
+  "use_namespace"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/sofa/build/build/install"
+  "/sofa/build/cmake/include"
+  "../IABPLUGIN_INCLUDE"
+  "../plugins"
+  "../plugins/IABPlugin/include"
+  "/sofa/build/install/include/SofaGui"
+  "/sofa/build/install/include/SofaGeneral"
+  "/sofa/build/install/include/extlibs/Eigen"
+  "/sofa/build/install/include/extlibs/json"
+  "/sofa/build/install/include/SofaFramework"
+  "/sofa/build/install/include/extlibs/TinyXML"
+  "/sofa/build/install/include/extlibs/GTest/include"
+  "/sofa/build/install/include/SofaSimulation"
+  "/sofa/build/install/include/SofaBase"
+  "/sofa/build/install/include/SofaCommon"
+  "/sofa/build/install/include/extlibs/Newmat"
+  "/sofa/build/install/include/SofaAdvanced"
+  "/sofa/build/install/include/SofaMisc"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCharts"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/sofa/build/install/include/extlibs/QGLViewer"
+  "/usr/include/x86_64-linux-gnu/qt5/QtXml"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ sofa/build/build/libs/CMakeFiles/IABPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "include/moc_DataWidgetUnsigned.cpp"
+  "CMakeFiles/IABPlugin.dir/include/moc_DataWidgetUnsigned.cpp.o"
+  "CMakeFiles/IABPlugin.dir/src/initIABPlugin.cpp.o"
+  "CMakeFiles/IABPlugin.dir/src/BehaviorModel.cpp.o"
+  "CMakeFiles/IABPlugin.dir/src/DataWidgetUnsigned.cpp.o"
+  "CMakeFiles/IABPlugin.dir/src/MappingPendulumInPlane.cpp.o"
+  "CMakeFiles/IABPlugin.dir/src/ProjectiveConstraintSet.cpp.o"
+  "../../lib/libIABPlugin.pdb"
+  "../../lib/libIABPlugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IABPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
