30ab927a3a4ac1ea77bbe46b451c5a4f581b4f83
lakehanne <patlekano@gmail.com>
2019-12-02 21:03:31 -0500

    continue in morning

--- sofa/src/pneunets.py
+++ sofa/src/pneunets.py
@@ -19,6 +19,7 @@ translateFinger3 = "0 " + str(radius + radius*math.sin(angle2-math.pi/2)) + " " 
 def createScene(rootNode):
     rootNode.createObject('RequiredPlugin', pluginName='SoftRobots')
     rootNode.createObject('RequiredPlugin', pluginName='SofaOpenglVisual')
+    rootNode.createObject('RequiredPlugin', pluginName='SofaSparseSolver')
     rootNode.createObject('VisualStyle', displayFlags='showVisualModels hideBehaviorModels hideCollisionModels hideBoundingCollisionModels hideForceFields showInteractionForceFields hideWireframe')
     rootNode.findData('gravity').value='-9810 0 0';
     rootNode.createObject('FreeMotionAnimationLoop')
@@ -33,13 +34,13 @@ def createScene(rootNode):
     rootNode.createObject('PythonScriptController', filename="controllerGripper.py", classname="controller")
 
     planeNode = rootNode.createChild('Plane')
-    planeNode.createObject('MeshObjLoader', name='loader', filename="mesh/floorFlat.obj", triangulate="true", rotation="0 0 270", scale =10, translation="-122 0 0")
-    planeNode.createObject('Mesh', src="@loader")
+    planeNode.createObject('MeshObjLoader', name='planeloader', filename="mesh/floorFlat.obj", triangulate="true", rotation="0 0 270", scale =10, translation="-122 0 0")
+    planeNode.createObject('Mesh', src="@planeloader")
     planeNode.createObject('MechanicalObject', src="@loader")
     planeNode.createObject('Triangle', simulated="0", moving="0")
     planeNode.createObject('Line', simulated="0", moving="0")
     planeNode.createObject('Point', simulated="0", moving="0")
-    planeNode.createObject('OglModel',name="floorVisual", src="@floorLoader", color="1 0 0 1",rotation="0 0 270", scale =10, translation="-122 0 0")
+    planeNode.createObject('OglModel',name="@planeloader", color="1 0 0 1",rotation="0 0 270", scale =10, translation="-122 0 0")
 
     cube = rootNode.createChild('cube')
     cube.createObject('EulerImplicit', name='odesolver')
@@ -50,8 +51,8 @@ def createScene(rootNode):
 
     #collision
     cubeCollis = cube.createChild('cubeCollis')
-    cubeCollis.createObject('MeshObjLoader', name="loader", filename="mesh/smCube27.obj", triangulate="true",  scale="6")
-    cubeCollis.createObject('Mesh', src="@loader")
+    cubeCollis.createObject('MeshObjLoader', name="cubeLoader", filename="mesh/smCube27.obj", triangulate="true",  scale="6")
+    cubeCollis.createObject('Mesh', src="@cubeLoader")
     cubeCollis.createObject('MechanicalObject', translation='0 0 0')
     cubeCollis.createObject('Triangle')
     cubeCollis.createObject('Line')
@@ -104,7 +105,7 @@ def createScene(rootNode):
     cavity.createObject('MeshSTLLoader', name='loader', filename=path+'pneunetCavityCut.stl',translation = translateFinger1)
     cavity.createObject('Mesh', src='@loader', name='topo')
     cavity.createObject('MechanicalObject', name='cavity')
-    cavity.createObject('SurfacePressureConstraint', name="SurfacePressureConstraint", template='Vec3d', value="0.0001", triangles='@topo.triangles', visualization='0', drawScale='0.0002', valueType="pressure")
+    cavity.createObject('SurfacePressureConstraint', name="SurfacePressureConstraint", template='Vec3d', value="0.0001", triangles='@topo.triangles', drawPressure='0', drawScale='0.0002', valueType="pressure")
     cavity.createObject('BarycentricMapping', name='mapping',  mapForces='false', mapMasses='false')
 
     ##########################################
@@ -125,7 +126,7 @@ def createScene(rootNode):
     # Visualization                          #
     ##########################################
     modelVisu = finger1.createChild('visu')
-    modelVisu.createObject('OglModel', name="pneuLoader", template='ExtVec3f', color='0.7 0.7 0.7 0.6',translation = translateFinger1)
+    modelVisu.createObject('OglModel', name="pneuLoader", src="@pneuLoader", template='Vec3d', color='0.7 0.7 0.7 0.6',translation = translateFinger1)
     modelVisu.createObject('BarycentricMapping')
 
     ##########################################
@@ -160,8 +161,8 @@ def createScene(rootNode):
     # Constraint                             #
     ##########################################
     cavity = finger2.createChild('cavity')
-    cavity.createObject('MeshSTLLoader', name='cavLoader', filename=path+'pneunetCavityCut.stl',rotation=str(360 - angle1*180/math.pi)+ " 0 0" , translation=translateFinger2)
-    cavity.createObject('Mesh', src='@loader', name='topo')
+    cavity.createObject('MeshSTLLoader', name='cavloader', filename=path+'pneunetCavityCut.stl',rotation=str(360 - angle1*180/math.pi)+ " 0 0" , translation=translateFinger2)
+    cavity.createObject('Mesh', src='@cavloader', name='topo')
     cavity.createObject('MechanicalObject', name='cavity')
     cavity.createObject('SurfacePressureConstraint', name="SurfacePressureConstraint", template='Vec3d', value="0.0001", triangles='@topo.triangles', drawPressure='0', drawScale='0.0002', valueType="pressure")
     cavity.createObject('BarycentricMapping', name='mapping',  mapForces='false', mapMasses='false')
@@ -184,7 +185,7 @@ def createScene(rootNode):
     # Visualization                          #
     ##########################################
     modelVisu = finger2.createChild('visu')
-    modelVisu.createObject('OglModel', name="cavVizLoader", filename="@cavLoader", template='ExtVec3f', color='0.7 0.7 0.7 0.6',rotation=str(360 - angle1*180/math.pi)+ " 0 0", translation=translateFinger2)
+    modelVisu.createObject('OglModel', name="cavloader", src="@cavloader", template='Vec3d', color='0.7 0.7 0.7 0.6',rotation=str(360 - angle1*180/math.pi)+ " 0 0", translation=translateFinger2)
     modelVisu.createObject('BarycentricMapping')
 
     ##########################################
@@ -222,7 +223,7 @@ def createScene(rootNode):
     cavity.createObject('MeshSTLLoader', name='loader', filename=path+'pneunetCavityCut.stl',rotation=str(360 - angle2*180/math.pi)+ " 0 0", translation=translateFinger3)
     cavity.createObject('Mesh', src='@loader', name='topo')
     cavity.createObject('MechanicalObject', name='cavity')
-    cavity.createObject('SurfacePressureConstraint', name="SurfacePressureConstraint", template='Vec3d', value="0.0001", triangles='@topo.triangles', visualization='0', drawScale='0.0002', valueType="pressure")
+    cavity.createObject('SurfacePressureConstraint', name="SurfacePressureConstraint", template='Vec3d', value="0.0001", triangles='@topo.triangles', drawPressure='0', drawScale='0.0002', valueType="pressure")
     cavity.createObject('BarycentricMapping', name='mapping',  mapForces='false', mapMasses='false')
 
     ##########################################
@@ -230,8 +231,8 @@ def createScene(rootNode):
     ##########################################
 
     collisionFinger = finger3.createChild('collisionFinger')
-    collisionFinger.createObject('MeshSTLLoader', name='loader', filename=path+'pneunetCut.stl',rotation=str(360 - angle2*180/math.pi)+ " 0 0", translation=translateFinger3)
-    collisionFinger.createObject('Mesh', src='@loader', name='topo')
+    collisionFinger.createObject('MeshSTLLoader', name='collisloader', filename=path+'pneunetCut.stl',rotation=str(360 - angle2*180/math.pi)+ " 0 0", translation=translateFinger3)
+    collisionFinger.createObject('Mesh', src='@collisloader', name='topo')
     collisionFinger.createObject('MechanicalObject', name='collisMech')
     collisionFinger.createObject('Triangle', selfCollision="false")
     collisionFinger.createObject('Line', selfCollision="false")
@@ -243,7 +244,7 @@ def createScene(rootNode):
     # Visualization                          #
     ##########################################
     modelVisu = finger3.createChild('visu')
-    modelVisu.createObject('OglModel', name="modelLoader", template='Vec3f', color='0.7 0.7 0.7 0.6',rotation=str(360 - angle2*180/math.pi)+ " 0 0", translation=translateFinger3)
+    modelVisu.createObject('OglModel', name="collisloader", template='Vec3d', color='0.7 0.7 0.7 0.6',rotation=str(360 - angle2*180/math.pi)+ " 0 0", translation=translateFinger3)
     modelVisu.createObject('BarycentricMapping')
 
     return rootNode
